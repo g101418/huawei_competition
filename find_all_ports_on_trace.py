13181857de9d246fa4493add60b6fbea8dc2dbc0
Gao Shijie <1014186239@qq.com>
2020-08-05 05:05:26 +0800

    bug

--- find_all_ports_on_trace.py
+++ find_all_ports_on_trace.py
@@ -214,7 +214,7 @@ class FindPorts(object):
                 
                 port_df_speed_is_0 = port_df[port_df['speed'] == 0]
                 
-                if len(port_df_speed_is_0) < 2:
+                if len(port_df_speed_is_0) < 2 or len(port_df) < 2:
                     continue
                 
                 port_df_speed_is_0 = port_df.loc[port_df_speed_is_0.index[0]: port_df_speed_is_0.index[-1]]
