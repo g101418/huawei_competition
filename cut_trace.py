383ab8201d24ad712ea0b81bccc182ad4ea2681b
Gao Shijie <1014186239@qq.com>
2020-08-06 16:17:46 +0800

    只取头尾节点匹配的航线

--- cut_trace.py
+++ cut_trace.py
@@ -62,6 +62,8 @@ class CutTrace(object):
             if len(self.__orders_ports_name_dict[key]) < 2:
                 continue
             if start_port in self.__orders_ports_name_dict[key] and end_port in self.__orders_ports_name_dict[key]:
+                if start_port != self.__orders_ports_name_dict[key][0][0] or end_port != self.__orders_ports_name_dict[key][-1][0]:
+                    continue
                 # 起止港均在内
                 start_indexs = [i for i in range(len(
                     self.__orders_ports_name_dict[key])) if self.__orders_ports_name_dict[key][i] == start_port]
